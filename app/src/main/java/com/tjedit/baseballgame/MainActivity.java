27a6e0347dc48730b26e397fa36f652093bf1a28
user <kyronko@naver.com>
2019-05-18 16:13:29 +0900

    사용자 입력값을 ?S ?B 인지 판정

--- app/src/main/java/com/tjedit/baseballgame/MainActivity.java
+++ app/src/main/java/com/tjedit/baseballgame/MainActivity.java
@@ -3,6 +3,10 @@ package com.tjedit.baseballgame;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.tjedit.baseballgame.databinding.ActivityMainBinding;
 
@@ -20,7 +24,39 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
-
+        act.inputBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkStrikeAndBalls();
+            }
+        });
+    }
+    void checkStrikeAndBalls(){
+        int[] userinputArray = new int[3];
+        int number = Integer.parseInt(act.userInputEdt.getText().toString()) ;
+         userinputArray[0] = number/100;
+        userinputArray[1] = number / 10 % 10;
+        userinputArray[2] = number % 10;
+         int strikeCount =0;
+         int ballCount =0;
+        for(int i=  0; i<3;i++){
+            for(int j= 0 ; j<3; j++){
+                if(userinputArray[i]==computerExamArray[j]){
+                    if(i==j){
+                        strikeCount++;
+                    }
+                    else{
+                        ballCount++;
+                    }
+                }
+            }
+        }
+        if(strikeCount==3){
+            Toast.makeText(mContext, "정답입니다! 축하합니다!", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(mContext, String.format("%d S , %d B입니다",strikeCount,ballCount), Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -53,6 +89,8 @@ public class MainActivity extends BaseActivity {
                 computerExamArray[0] = tempNumber[0];
                 computerExamArray[1] = tempNumber[1];
                 computerExamArray[2] = tempNumber[2];
+
+                Log.d("정답숫자",randomNumber+"입니다");
                 break;
 //                문제 없으면 while문 탈출
             }
