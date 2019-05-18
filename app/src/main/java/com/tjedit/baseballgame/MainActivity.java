99c5ec233b804bf87dd6c416e5a8a7a66d420ea7
user <kyronko@naver.com>
2019-05-18 15:58:26 +0900

    문제출제로직완성

--- app/src/main/java/com/tjedit/baseballgame/MainActivity.java
+++ app/src/main/java/com/tjedit/baseballgame/MainActivity.java
@@ -13,9 +13,9 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         bindViews();
-         setupEvents();
-         setupValues();
+        bindViews();
+        setupEvents();
+        setupValues();
     }
 
     @Override
@@ -25,11 +25,43 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupValues() {
+        makeExam();
+
+    }
+
+    void makeExam() {
+        while (true) {
+//        문제를 출제하는 부분
+            int randomNumber = (int) Math.random() * 899 + 100;
+//        각각의 자리수를 쪼개서 배열에 나눠 담아두기
+            int[] tempNumber = new int[3];
+            tempNumber[0] = randomNumber / 100; //100자리
+            tempNumber[1] = randomNumber / 10 % 10; //10자리
+            tempNumber[2] = randomNumber % 10;  //1자리
+
+            boolean isDuplOk = true;
+            if (tempNumber[0] == tempNumber[1] || tempNumber[1] == tempNumber[2] || tempNumber[0] == tempNumber[2]) {
+//            같은숫자가 있을시 다시출제
+                isDuplOk = false;
+            }
+            boolean isZeroOk = true;
+            if (tempNumber[0] == 0 || tempNumber[1] == 1 || tempNumber[2] == 0) {
+//        0이 들어있을시 다시 출제
+                isZeroOk = false;
+            }
+            if (isDuplOk && isZeroOk) {
+                computerExamArray[0] = tempNumber[0];
+                computerExamArray[1] = tempNumber[1];
+                computerExamArray[2] = tempNumber[2];
+                break;
+//                문제 없으면 while문 탈출
+            }
+        }
 
     }
 
     @Override
     public void bindViews() {
-        act = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        act = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
