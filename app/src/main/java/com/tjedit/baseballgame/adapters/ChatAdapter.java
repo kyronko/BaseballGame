ec1f2a298aff2d7f2930c6a09f9536ece0dcd709
user <kyronko@naver.com>
2019-05-19 10:19:29 +0900

    어댑터

---
+++ app/src/main/java/com/tjedit/baseballgame/adapters/ChatAdapter.java
@@ -0,0 +1,42 @@
+package com.tjedit.baseballgame.adapters;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.tjedit.baseballgame.R;
+import com.tjedit.baseballgame.datas.Chat;
+
+import java.util.List;
+
+public class ChatAdapter extends ArrayAdapter<Chat> {
+    Context mContext;
+    List<Chat> mList;
+    LayoutInflater inf;
+
+    public ChatAdapter(Context context , List<Chat> list){
+        super(context, R.layout.chat_list_item,list);
+
+        mContext = context;
+        mList = list;
+        inf = LayoutInflater.from(mContext);
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        if(row==null)
+        {
+            row= inf.inflate(R.layout.chat_list_item,null);
+        }
+
+        return  row;
+    }
+
+}
